f5b10b3e4e2137f0f7ddaece733e8c292a1505e3
MasafumiYamamoto <mythmaker3633@yahoo.co.jp>
2015-11-20 01:23:34 +0900

    chagne busclus
    単語のtfidfのみだすように変更

--- busclusword.py
+++ busclusword.py
@@ -1,75 +1,74 @@
-import csv
-import os
-import collections
-import textedit
-import numpy
-
-busname="IjcKyB-LUnev2iapzyJ11w"
-
-pas="D:/Lresult"
-pas2="//kaede/PPTShare/masafumi/musc/20151117"
-ifile=open(pas+"/ks/busclus/"+busname+".csv","r")
-idata=csv.reader(ifile)
-idata.next()
-cluslist=collections.Counter()
-for line in idata:
-    ####revid,sent,clus
-    cluslist[line[0],line[1]]=int(line[2])
-ifile.close()
-ifile=open(pas2+"/ks/bussent/"+busname+".csv","r")
-idata=csv.reader(ifile)
-wlist=collections.Counter()
-for num in range(0,10):
-    wlist[num]=collections.Counter()
-for line in idata:
-    doc=textedit.textedit(line[5])
-    doc=doc.split()
-    for t in doc:
-        wlist[cluslist[line[0],line[6]]][t]=wlist[cluslist[line[0],line[6]]][t]+1
-ifile.close()
-
-tflist=collections.Counter()
-wordlen=[]
-wordsum=collections.Counter()
-for t in range(0,10):
-    wordlen.append(len(wlist[t]))
-    wordsum[t]=sum(wlist[t].values())
-    for w in wlist[t]:
-        tflist[w]=tflist[w]+1
-print wordlen,max(wordlen)
-
-#wfile=open(pas+"/ks/busclusword/"+busname+".csv","wb")
-wfile=open(pas+"/ks/busclusword/hoge.csv","wb")
-writer=csv.writer(wfile)
-header=[]
-for num in map(str,range(0,10)):
-    header=header+["c"+num+"word","c"+num+"num","c"+num+"tfidf"]
-writer.writerow(header)
-for num in range(0,max(wordlen)):
-    wwlist=[]
-    for t in range(0,10):
-        if(len(wlist[t])<=num):
-            wwlist=wwlist+[" "," "," "]
-        else:
-            tmp=wlist[t].items()
-            wwlist.append(tmp[num][0])
-            wwlist.append(tmp[num][1])
-            wwlist.append(1.0*tmp[num][1]/wordsum[t]*numpy.log(10/tflist[tmp[num][0]]))
-    writer.writerow(wwlist)
-'''
-for t in wlist:
-    wwlist=["clus","word"]
-    wwlist=wwlist+wlist[t].keys()
-    writer.writerow(wwlist)
-    wwlist=[t,"num"]
-    wwlist=wwlist+wlist[t].values()
-    writer.writerow(wwlist)
-    wwlist=[t,"tfidf"]
-    for w in wlist[t]:
-        wwlist.append(1.0*wlist[t][w]/wordsum*numpy.log(10/tflist[w]))
-    writer.writerow(wwlist)
-    #writer.writerow([t,"sum",wordsum])
-'''
 
 
-wfile.close()
+def main(cluster,fwords):
+    import csv
+    import os
+    import collections
+    import textedit
+    import numpy
+
+    busname="4bEjOyTaDG24SY5TxsaUNQ"
+    pas="D:/Lresult"
+    pas2="//kaede/PPTShare/masafumi/musc/20151117"
+    fewords=int(fwords)####feature word num
+    cnum=int(cluster)###cluster_num
+
+    ifile=open(pas+"/ks/busclus/"+busname+".csv","r")
+    idata=csv.reader(ifile)
+    idata.next()
+    cluslist=collections.Counter()
+    for line in idata:
+        ####revid,sentiment_num,clus
+        cluslist[line[0],line[1]]=int(line[2])
+    ifile.close()
+    ifile=open(pas2+"/ks/bussent/"+busname+".csv","r")
+    idata=csv.reader(ifile)
+    wlist=collections.Counter()####word dictionary for each cluster
+    for num in range(0,cnum):
+        wlist[num]=collections.Counter()
+    for line in idata:
+        doc=textedit.textedit(line[5])
+        doc=doc.split()
+        for t in doc:
+            wlist[cluslist[line[0],line[6]]][t]=wlist[cluslist[line[0],line[6]]][t]+1
+    ifile.close()
+
+    tflist=collections.Counter()
+    wordlen=[]
+    wordsum=collections.Counter()
+    for t in range(0,cnum):
+        wordlen.append(len(wlist[t]))
+        wordsum[t]=sum(wlist[t].values())
+        for w in wlist[t]:
+            tflist[w]=tflist[w]+1
+    print wordlen,max(wordlen)
+
+    wfile=open(pas+"/ks/busclusword/"+busname+".csv","wb")
+    writer=csv.writer(wfile)
+    header=[]
+    for num in map(str,range(0,cnum)):
+        header=header+["c"+num+"word","c"+num+"tfidf"]
+    writer.writerow(header)
+
+    toplist=collections.Counter()
+    for t in range(0,cnum):
+        tmp=collections.Counter()
+        for w in wlist[t]:
+            tmp[w]=1.0*wlist[t][w]/wordsum[t]*numpy.log(1.0*cnum/tflist[w])
+        toplist[t]=tmp.most_common(fewords)
+
+    for num in range(0,cnum):
+        wwlist=[]
+        for t in range(0,fewords):
+            if(len(wlist[t])<=num):
+                wwlist=wwlist+["_",0]
+            else:
+                tmp=toplist[t]
+                wwlist.append(tmp[num][0])
+                wwlist.append(tmp[num][1])
+        writer.writerow(wwlist)
+    wfile.close()
+
+if __name__ == '__main__':
+    print "cluster_num,feature_words"
+    main(10,10)
--- input_kmeans.py
+++ input_kmeans.py
@@ -1,72 +1,78 @@
-import numpy
-import csv
-from sklearn.cluster import KMeans
-import collections
-import glob
-import os
-import numpy
-import gensim
-import textedit
-import time
 
-pas="D:/Lresult"
-model="lda"
-tnum=10
-cnum=100
+def main(model_,topic,cluster):
+	import numpy
+	import csv
+	from sklearn.cluster import KMeans
+	import collections
+	import os
+	import numpy
+	import gensim
+	import textedit
+	import time
 
-####prepare model
-lmodel=gensim.models.LdaModel.load(pas+"/model/"+model+"model/NVreview_o4b6t"+str(tnum)+".lda")
-dictionary=gensim.corpora.Dictionary.load(pas+"/model/dict/NVreview_o4b6.dict")
+	pas="D:/Lresult"
+	model=model_
+	tnum=int(topic)
+	cnum=int(cluster)
 
-##load stoplist
-stoplist=collections.Counter()
-stopfile=open("D:/Lresult/stopwords/over4word.csv","r")
-stopdata=csv.reader(stopfile)
-for line in stopdata:
-	stoplist[line[0]]=1
-stopfile.close()
-stopfile=open("D:/Lresult/stopwords/b6list.csv","r")
-stopdata=csv.reader(stopfile)
-for line in stopdata:
-	stoplist[line[0]]=1
-stopfile.close()
-stopset=set(stoplist)
-print "stop fin",len(stoplist),time.ctime()
+	####prepare model
+	lmodel=gensim.models.LdaModel.load(pas+"/model/"+model+"model/NVreview_o4b6t"+str(tnum)+"."+model)
+	dictionary=gensim.corpora.Dictionary.load(pas+"/model/dict/NVreview_o4b6.dict")
 
-#ifile=open(pas+"/ks/bussent/_L1SVry9jDzk6VLz75Z6Ow.csv","r")
-busname="IjcKyB-LUnev2iapzyJ11w"
-ifile=open("//kaede/PPTShare/masafumi/musc/20151117/ks/bussent/"+busname+".csv","r")
-idata=csv.reader(ifile)
-idata.next()
-revlist=collections.Counter()
-revvec=collections.Counter()
-revlen=collections.Counter()
-n=0
-for line in idata:
-	doc=textedit.textedit(line[5])
-	doc=doc.lower().split()
-	docset=set(doc)-stopset
-	vec_bow = dictionary.doc2bow(docset)
-	vec_lmodel = lmodel[vec_bow]
-	rlen=0
-	revlist[line[0],line[6]]=[0]*tnum
-	for num in range(0,len(vec_lmodel)):
-		rlen=rlen+vec_lmodel[num][1]*vec_lmodel[num][1]
-		revlist[line[0],line[6]][vec_lmodel[num][0]]=vec_lmodel[num][1]
-	#revlen[line[0],line[6]]
-	rlen=numpy.sqrt(rlen)
-	#print revlist[line[0],line[6]]
-	revlist[line[0],line[6]]=revlist[line[0],line[6]]/rlen
-	#print revlist[line[0],line[6]]
-#print len(revlist)/3
+	##load stoplist
+	stoplist=collections.Counter()
+	stopfile=open("D:/Lresult/stopwords/over4word.csv","r")
+	stopdata=csv.reader(stopfile)
+	for line in stopdata:
+		stoplist[line[0]]=1
+	stopfile.close()
+	stopfile=open("D:/Lresult/stopwords/b6list.csv","r")
+	stopdata=csv.reader(stopfile)
+	for line in stopdata:
+		stoplist[line[0]]=1
+	stopfile.close()
+	stopset=set(stoplist)
+	print "stop fin",len(stoplist),time.ctime()
 
-kdata=numpy.array(revlist.values())
-kmeans_model=KMeans(n_clusters=10,random_state=1).fit(kdata)
-labels=kmeans_model.labels_
+	#ifile=open(pas+"/ks/bussent/_L1SVry9jDzk6VLz75Z6Ow.csv","r")
+	busname="4bEjOyTaDG24SY5TxsaUNQ"
+	ifile=open("//kaede/PPTShare/masafumi/musc/20151117/ks/bussent/"+busname+".csv","r")
+	idata=csv.reader(ifile)
+	idata.next()
+	revlist=collections.Counter()###review cluster
+	revvec=collections.Counter()###review vector
+	revlen=collections.Counter()###length of review vector
+	n=0
+	for line in idata:
+		doc=textedit.textedit(line[5])
+		doc=doc.lower().split()
+		docset=set(doc)-stopset###remove stopwords
+		vec_bow = dictionary.doc2bow(docset)
+		vec_lmodel = lmodel[vec_bow]
+		rlen=0
+		revlist[line[0],line[6]]=[0]*tnum
+		for num in range(0,len(vec_lmodel)):
+			rlen=rlen+vec_lmodel[num][1]*vec_lmodel[num][1]
+			revlist[line[0],line[6]][vec_lmodel[num][0]]=vec_lmodel[num][1]
+		rlen=numpy.sqrt(rlen)
+		if(rlen!=0):
+			revlist[line[0],line[6]]=revlist[line[0],line[6]]/rlen
+		#print revlist[line[0],line[6]]
+	ifile.close()
 
-wfile=open(pas+"/ks/busclus/"+busname+".csv","wb")
-writer=csv.writer(wfile)
-writer.writerow(["revid","sent","clus"])
+	##k-means
+	kdata=numpy.array(revlist.values())
+	kmeans_model=KMeans(n_clusters=cnum,random_state=1).fit(kdata)
+	labels=kmeans_model.labels_
 
-for num in range(0,len(revlist)):
-	writer.writerow(list(revlist.keys()[num])+[labels[num]])
+	wfile=open(pas+"/ks/busclus/"+busname+".csv","wb")
+	writer=csv.writer(wfile)
+	writer.writerow(["revid","sent","clus"])
+	for num in range(0,len(revlist)):
+		writer.writerow(list(revlist.keys()[num])+[labels[num]])
+	wfile.close()
+	print "fin",time.ctime()
+
+if __name__ == '__main__':
+	print "lmodel","topic_num","cluster_num"
+	main("lda",500,10)
